b9d822b50e7457fe1acc7ec0d21e1379edd64ca4
Jáchym Toušek <enumag@gmail.com>
2015-01-03 22:32:22 +0100

    Fixed namespaces

--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the file license.md that was distributed with this source code.
  */
 
-namespace Arachne\SessionConverter;
+namespace Arachne\SessionConverter\Exception;
 
 /**
  * The exception that is thrown when an argument does not match with the expected value.
