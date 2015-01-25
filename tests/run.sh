ed8813835a1688f97990c0e5313cd2f95879c8a1
Jáchym Toušek <enumag@gmail.com>
2015-01-25 21:31:10 +0100

    Removed tests directory

--- tests/run.sh
+++
@@ -1,3 +0,0 @@
-./vendor/bin/phpcs -p --standard=vendor/arachne/coding-style/ruleset.xml src
-./vendor/bin/phpcs -p --standard=vendor/arachne/coding-style/ruleset.xml --ignore=_temp tests
-./vendor/bin/codecept build
